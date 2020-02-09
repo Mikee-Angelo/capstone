21ac3163abf1fc2475a669855cfd4eddefea0850
Michael Angelo De Leon <michael19972910@gmail.com>
2020-02-09 20:59:32 +0800

    fix update

--- application/config/form_validation.php
+++ application/config/form_validation.php
@@ -2,13 +2,15 @@
 
 $config = [
     'login' => [
-        'field' => 'user', 
-        'label' => 'ID Number',
-        'rules' => 'required'
-    ],
-    'password' => [
-        'field' => 'pwd', 
-        'label' => 'Password',
-        'rules' => 'required|min_length[8]'
+        'user' => [
+            'field' => 'user', 
+            'label' => 'ID Number',
+            'rules' => 'required'
+        ],
+        'password' => [
+            'field' => 'pwd', 
+            'label' => 'Password',
+            'rules' => 'required|min_length[8]'
+        ],
     ],
 ];
